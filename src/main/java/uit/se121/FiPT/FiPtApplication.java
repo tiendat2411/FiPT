17135588bf1086d9a92a5b7ded4cdb18170e94a0
tiendat2411 <datsetoit1@gmail.com>
2024-10-23 09:59:18 +0700

    Initial commit

---
+++ src/main/java/uit/se121/FiPT/FiPtApplication.java
@@ -0,0 +1,13 @@
+package uit.se121.FiPT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiPtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiPtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uit/se121/FiPT/FiPtApplicationTests.java
@@ -0,0 +1,13 @@
+package uit.se121.FiPT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiPtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
