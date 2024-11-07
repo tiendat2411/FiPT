cb31c7ac6f7e3b06ef568e195f2b4ea3e2dc78ce
tiendat2411 <datsetoit1@gmail.com>
2024-11-07 09:55:26 +0700

    fix some error

---
+++ src/main/java/uit/se121/FiPT/service/UserService.java
@@ -0,0 +1,86 @@
+package uit.se121.FiPT.service;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import uit.se121.FiPT.dto.request.UserCreationRequest;
+import uit.se121.FiPT.dto.request.UserUpdateRequest;
+import uit.se121.FiPT.dto.response.UserResponse;
+import uit.se121.FiPT.entity.User;
+import uit.se121.FiPT.enums.Role;
+import uit.se121.FiPT.exception.AppException;
+import uit.se121.FiPT.exception.ErrorCode;
+import uit.se121.FiPT.mapper.UserMapper;
+import uit.se121.FiPT.repository.UserRepository;
+
+
+import java.util.HashSet;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@Slf4j
+public class UserService {
+    UserMapper userMapper;
+    UserRepository userRepository;
+    PasswordEncoder passwordEncoder;
+
+    public UserResponse createUser(UserCreationRequest request){
+        if (userRepository.existsByUsername(request.getUsername()))
+            throw new AppException(ErrorCode.USER_EXISTED);
+
+        User user = userMapper.toUser(request);
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+
+        HashSet<String> roles = new HashSet<>();
+        roles.add(Role.USER.name());
+
+        user.setRoles(roles);
+
+        return userMapper.toUserResponse(userRepository.save(user));
+    }
+
+    public UserResponse getMyInfo(){
+        var context = SecurityContextHolder.getContext();
+        String name = context.getAuthentication().getName();
+
+        User user = userRepository.findByUsername(name).orElseThrow(
+                () -> new AppException(ErrorCode.USER_NOT_EXISTED));
+
+        return userMapper.toUserResponse(user);
+    }
+
+    public UserResponse updateUser(String userId, UserUpdateRequest request) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_EXISTED));
+
+        userMapper.updateUser(user, request);
+
+        return userMapper.toUserResponse(userRepository.save(user));
+    }
+
+    public void deleteUser(String userId){
+        userRepository.deleteById(userId);
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    public List<UserResponse> getUsers(){
+        log.info("In method get Users");
+        return userRepository.findAll().stream()
+                .map(userMapper::toUserResponse).toList();
+    }
+
+    @PostAuthorize("returnObject.username == authentication.name")
+    public UserResponse getUser(String id){
+        log.info("In method get user by Id");
+        return userMapper.toUserResponse(userRepository.findById(id)
+                .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_EXISTED)));
+    }
+}
